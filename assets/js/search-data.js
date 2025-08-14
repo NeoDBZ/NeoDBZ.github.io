c72cb83350e37ad9a9ad74d4389d4ff184e58870
NeoDBZ <fabledvice@aol.com>
2025-08-14 07:24:31 +0000

    Deploying to gh-pages from @ NeoDBZ/NeoDBZ.github.io@a8a0ca535165c92032dc03be97a1121424aed5c5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-about2",
+    title: "about2",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
